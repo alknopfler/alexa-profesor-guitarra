fe9902798ae0fbfd77cf23b4672271be7d791f85
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-11 11:53:35 +0100

    add dialogue

--- functions/acorde.go
+++ functions/acorde.go
@@ -4,12 +4,23 @@ import (
 	"github.com/ericdaugherty/alexa-skills-kit-golang"
 	cfg "github.com/alknopfler/alexa-profesor-guitarra/config"
 	"context"
+	"log"
 )
 
 func AcordeIntent(context context.Context, request *alexa.Request, session *alexa.Session, aContext *alexa.Context, response *alexa.Response) {
-	response.SetStandardCard(cfg.CardTitle, cfg.SpeechCancel, cfg.ImageSmall, cfg.ImageLong)
-	response.SetOutputText("El acorde elegido es: "+request.Intent.Slots["acorde"].Value+request.Intent.Slots["tipo"].Value)
-	response.ShouldSessionEnd = true
-	return
 
+
+
+	if request.DialogState != "COMPLETED" {
+		log.Println("Get into dialog to confirm name 'acordeIntent'")
+		response.AddDialogDirective("Dialog.Delegate", "", "", &request.Intent)
+		response.ShouldSessionEnd = false
+
+	} else {
+
+		response.SetStandardCard(cfg.CardTitle, cfg.SpeechCancel, cfg.ImageSmall, cfg.ImageLong)
+		response.SetOutputText("El acorde elegido es: " + request.Intent.Slots["acorde"].Value + request.Intent.Slots["tipo"].Value)
+		response.ShouldSessionEnd = true
+		return
+	}
 }
