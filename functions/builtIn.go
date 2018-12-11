06cf114d6afb4f4728b2b6871f46b9355bc6d46c
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-11 12:10:54 +0100

    change the builtin config

--- functions/builtIn.go
+++ functions/builtIn.go
@@ -3,7 +3,7 @@ package function
 import (
 	"context"
 	"github.com/ericdaugherty/alexa-skills-kit-golang"
-	cfg "github.com/alknopfler/alexa-cenas/config"
+	cfg "github.com/alknopfler/alexa-profesor-guitarra/config"
 )
 
 func Cancel(context context.Context, request *alexa.Request, session *alexa.Session, aContext *alexa.Context, response *alexa.Response) {
