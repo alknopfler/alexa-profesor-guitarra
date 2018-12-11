4a9d155e6f73c5d23b2ac5742f1a05784bb585ac
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-11 12:13:33 +0100

    change message on launch

--- config/messages.go
+++ config/messages.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	SpeechOnLaunch 			= "Bienvenido al Profesor de guitarra. ¿Qué acorde quieres aprender hoy?"
+	SpeechOnLaunch 			= "Bienvenido al Profesor de guitarra. ¿Qué quieres aprender hoy?"
 	SpeechNavigate			= "Lo siento, pero esta funcionalidad no está implementada en el Profesor de Guitarra. Pídeme ayuda y te diré las opciones disponibles"
 	SpeechHelp				= "Puedes pedirme que te enseñe un acorde para poder practicarlo. ¿Qué acorder quieres aprender?"
 	SpeechCancel			= "Vale."
