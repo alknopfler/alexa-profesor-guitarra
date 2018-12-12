ca6fbfb5cab56500307d9f0e6458417feb341efb
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-12 18:11:50 +0100

    fifixx

--- config/messages.go
+++ config/messages.go
@@ -5,7 +5,7 @@ const (
 	SpeechNavigate			= "Lo siento, pero esta funcionalidad no está implementada en el Profesor de Guitarra. Pídeme ayuda y te diré las opciones disponibles"
 	SpeechHelp				= "Puedes pedirme que te enseñe a tocar un acorde para poder practicarlo. Te mostraré la digitación en una card y lo reproduciré para que puedas practicarlo. ¿Qué quieres aprender hoy?"
 	SpeechCancel			= "Vale."
-	SpeechAcorde			= "Vale, voy a mostrarte la digitación en una card y además, ahora voy a reproducirlo."
+	SpeechAcorde			= "Vale, voy a mostrarte la digitación en la pantalla o app Alexa y además, ahora voy a reproducirlo."
 	SpeechAcordeNotFound	= "Lo siento pero no he podido encontrar el acorde que me has pedido."
 
 	SpeechAmaj				= "La Mayor"
