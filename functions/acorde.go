9f33a166df994efa5aa85145780639319afb9a9f
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-12 17:52:57 +0100

    fix

--- functions/acorde.go
+++ functions/acorde.go
@@ -38,7 +38,6 @@ func AcordeIntent(context context.Context, request *alexa.Request, session *alex
 		response.SetStandardCard(cfg.CardTitle, acorde.text, acorde.image, acorde.image)
 		response.SetOutputText(cfg.SpeechAcorde)
 		response.AddAudioPlayer("AudioPlayer.Play","REPLACE_ALL", acorde.sound, acorde.sound,0)
-		response.ShouldSessionEnd = false
 		return
 	}
 }
