0dc72df30c1b45c0de0a5a23c8b7bbd1e97990c9
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-12 17:42:08 +0100

    trying to get session open after play chord

--- functions/acorde.go
+++ functions/acorde.go
@@ -38,7 +38,7 @@ func AcordeIntent(context context.Context, request *alexa.Request, session *alex
 		response.SetStandardCard(cfg.CardTitle, acorde.text, acorde.image, acorde.image)
 		response.SetOutputText(cfg.SpeechAcorde)
 		response.AddAudioPlayer("AudioPlayer.Play","REPLACE_ALL", acorde.sound, acorde.sound,0)
-		response.ShouldSessionEnd = true
+		response.SetRepromptText("Qué quieres hacer ahora?")
 		return
 	}
 }
