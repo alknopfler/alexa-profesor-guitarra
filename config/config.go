bf78e9a22d32cebd399c5ab0d4ffe5e9757e2287
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-12 13:46:04 +0100

    add sounds

--- config/config.go
+++ config/config.go
@@ -9,6 +9,22 @@ const(
 
 	AcordeIntent			= "acordeIntent"
 
+	SoundAmaj				= "https://feeds.soundcloud.com/stream/543793719-alberto-morgante-758035286-amaj.mp3"
+	SoundAmin				= "https://feeds.soundcloud.com/stream/543793707-alberto-morgante-758035286-amin.mp3"
+	SoundBmaj				= "https://feeds.soundcloud.com/stream/543793680-alberto-morgante-758035286-bmaj.mp3"
+	SoundBmin				= "https://feeds.soundcloud.com/stream/543793668-alberto-morgante-758035286-bmin.mp3"
+	SoundCmaj				= "https://feeds.soundcloud.com/stream/543793650-alberto-morgante-758035286-cmaj.mp3"
+	SoundCmin				= "https://feeds.soundcloud.com/stream/543793623-alberto-morgante-758035286-cmin.mp3"
+	SoundDmaj				= "https://feeds.soundcloud.com/stream/543793611-alberto-morgante-758035286-dmaj.mp3"
+	SoundDmin				= "https://feeds.soundcloud.com/stream/543793590-alberto-morgante-758035286-dmin.mp3"
+	SoundEmaj				= "https://feeds.soundcloud.com/stream/543793581-alberto-morgante-758035286-emaj.mp3"
+	SoundEmin				= "https://feeds.soundcloud.com/stream/543793569-alberto-morgante-758035286-emin.mp3"
+	SoundFmaj				= "https://feeds.soundcloud.com/stream/543793533-alberto-morgante-758035286-fmaj.mp3"
+	SoundFmin				= "https://feeds.soundcloud.com/stream/543793518-alberto-morgante-758035286-fmin.mp3"
+	SoundGmaj				= "https://feeds.soundcloud.com/stream/543793488-alberto-morgante-758035286-gmaj.mp3"
+	SoundGmin				= "https://feeds.soundcloud.com/stream/543793461-alberto-morgante-758035286-gmin.mp3"
+
+
 	Cancel					= "AMAZON.CancelIntent"
 	Stop					= "AMAZON.StopIntent"
 	Navigate				= "AMAZON.NavigateHomeIntent"
