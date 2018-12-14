e32f3ea5672a788ad02073f5df1bc00dc9caca1d
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-14 17:58:02 +0100

    upload new nodejs version

---
+++ constants.js
@@ -0,0 +1,95 @@
+/* CONSTANTS */
+
+exports.skill = {
+    appId: 'amzn1.ask.skill.99434a76-7b33-4725-8a5f-72cb85e0e470',
+    dynamoDBTableName: 'Profesor-guitarra',
+};
+
+exports.backtrackData = [
+    {
+        title: 'Still Got The Blues',
+        url: 'http://feeds.soundcloud.com/stream/544535421-alberto-morgante-758035286-still-got-the-blues.mp3',
+    },
+    {
+        title: 'Knockin on heavens door',
+        url: 'http://feeds.soundcloud.com/stream/544535235-alberto-morgante-758035286-knockin-on-heavens-door.mp3',
+    },
+    {
+        title: 'Canon Song',
+        url: 'http://feeds.soundcloud.com/stream/544535355-alberto-morgante-758035286-canon-rock.mp3',
+    },
+];
+
+exports.audioData = [
+    {
+        title: 'Do Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Cmaj.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793650-alberto-morgante-758035286-cmaj.mp3',
+    },
+    {
+        title: 'Do menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Cmin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793623-alberto-morgante-758035286-cmin.mp3',
+    },
+    {
+        title: 'Re Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Dmaj.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793611-alberto-morgante-758035286-dmaj.mp3',
+    },
+    {
+        title: 'Re menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Dmin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793590-alberto-morgante-758035286-dmin.mp3',
+    },
+    {
+        title: 'Mi Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Emaj.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793581-alberto-morgante-758035286-emaj.mp3',
+    },
+    {
+        title: 'Mi menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Emin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793569-alberto-morgante-758035286-emin.mp3',
+    },
+    {
+        title: 'Fa Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Fmaj.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793533-alberto-morgante-758035286-fmaj.mp3',
+    },
+    {
+        title: 'Fa menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Fmin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793518-alberto-morgante-758035286-fmin.mp3',
+    },
+    {
+        title: 'Sol Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Gmaj.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793488-alberto-morgante-758035286-gmaj.mp3',
+    },
+    {
+        title: 'Sol menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Gmin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793461-alberto-morgante-758035286-gmin.mp3',
+    },
+    {
+        title: 'La Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Amaj.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793719-alberto-morgante-758035286-amaj.mp3',
+    },
+    {
+        title: 'La menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Amin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793707-alberto-morgante-758035286-amin.mp3',
+    },
+    {
+        title: 'Si Mayor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Bjam.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793680-alberto-morgante-758035286-bmaj.mp3',
+    },
+    {
+        title: 'Si menor',
+        image: 'https://raw.githubusercontent.com/alknopfler/alexa-profesor-guitarra/master/images/Bmin.jpg',
+        url: 'https://feeds.soundcloud.com/stream/543793668-alberto-morgante-758035286-bmin.mp3',
+    },
+
+];
